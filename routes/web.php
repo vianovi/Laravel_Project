697c2af6d5e6659bf3c7963f2629760a61d211e3
vianovi <silviadewinovianaputri@gmail.com>
2025-10-26 07:31:39 +0700

    update pages component

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+// Generic route to serve views under resources/views/pages/**
+Route::get('/pages/{view?}', function (string $view = null) {
+    if (!$view) {
+        return redirect()->route('home');
+    }
+    $dot = str_replace('/', '.', $view);
+    $fullView = 'pages.' . $dot;
+    if (view()->exists($fullView)) {
+        return view($fullView);
+    }
+    abort(404);
+})->where('view', '.*')->name('pages.view');
